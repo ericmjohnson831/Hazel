84e74ac0bd2849645604505cc983ab654caf09fe
Eric Johnson <ejohnson@verb.local>
2019-11-17 21:42:38 -0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>
+
+
+// For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+// ---ENTRY POINT-----------------------------
+#include "Hazel/EntryPoint.h"
+// -------------------------------------------
